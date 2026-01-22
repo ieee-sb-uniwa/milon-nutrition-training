a57a4468f04fb9749507ad1098ab43837dfe1e1d
Damianzoub <d.zoumpos04@gmail.com>
2026-01-22 19:43:44 +0200

    fixed APIs backend

--- app/utils/valid_image.py
+++ app/utils/valid_image.py
@@ -1,6 +1,6 @@
 from fastapi import UploadFile, HTTPException
 FILE_TYPES = {".jpg",".jpeg",".png"}
-
+ 
 def validation_image_upload(file:UploadFile):
     if file is None:
         raise HTTPException(status_code=400,detail="No file uploaded")
